50b5f6d84e8fa162d1316fdd19811863fba20f7b
Sudhanshu jawanjal <sudhanshujawanjal007@gmail.com>
2026-06-17 18:21:41 +0530

    Implemented tasks creation with react state

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import BoardColumn from "@/components/BoardColumn";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
@@ -16,44 +19,36 @@ type Task = {
   status: "Todo" | "In Progress" | "Done";
 
 };
-const tasks: Task[] = [
-  {
+const [tasks, setTasks] = useState<Task[]>([
+   {
     title: "Learn React",
     priority: "High",
     dueDate: "2026-06-16",
     status: "Todo",
   },
   {
-    title: "Learn TypeScript",
-    priority: "Medium",
-    dueDate: "2026-06-17",
-    status: "Todo",
-  },
-  {
-    title: "Build Navbar",
-    priority: "Low",
-    dueDate: "2026-06-18",
-    status: "Todo",
-  },
-  {
     title: "Learn Prisma",
-    priority: "High",
-    dueDate: "2026-06-19",
-    status: "In Progress",
-  },
-  {
-    title: "Setup Database",
     priority: "Medium",
-    dueDate: "2026-06-20",
+    dueDate: "2026-06-18",
     status: "In Progress",
   },
   {
     title: "Build Portfolio",
     priority: "Low",
-    dueDate: "2026-06-21",
+    dueDate: "2026-06-20",
     status: "Done",
   },
-];
+]);
+
+function addTask() {
+  const newtask: Task = {
+    title: `Task ${tasks.length + 1}`,
+    priority: "High",
+    dueDate: "2026-06-25",
+    status: "Todo",
+};
+  setTasks([...tasks, newtask]);
+}
   return (
     <>
       <div className="flex  justify-center mt-4  ">
@@ -65,29 +60,33 @@ const tasks: Task[] = [
 
           <Sidebar />
     
-        <div className="p-4 flex gap-6  rounded-2xl text-center ">
-
-          {
-          columns.map((column) => (
-            <BoardColumn
-              key={column}
-              title={column}
-            >
+        <div className="p-4 flex flex-col gap-6 items-center   rounded-2xl text-center ">
+          <button onClick={addTask} className="flex gap-6  rounded-2xl text-center bg-[#8AB4F8] py-2 text-black font-semibold px-6 ">Add tasks</button>
+          <div className="flex gap-6  rounded-2xl text-center flex-col">
               {
-                 tasks
-                  .filter((task) => task.status === column)
-                  .map((task) => (
-                    <TaskCard
-                      key={task.title}
-                      title={task.title}
-                      priority={task.priority}
-                      dueDate={task.dueDate}
-                    />
-                  ))  
+                columns.map((column) => (
+                  <BoardColumn
+                    key={column}
+                    title={column}
+                  >
+                    {
+                      tasks
+                        .filter((task) => task.status === column)
+                        .map((task) => (
+                          <TaskCard
+                            key={task.title}
+                            title={task.title}
+                            priority={task.priority}
+                            dueDate={task.dueDate}
+                          />
+                        ))  
+                    }
+                  </BoardColumn>
+                ))
               }
-            </BoardColumn>
-          ))
-        }
+          </div>
+
+       
           
         </div>
       </div>
